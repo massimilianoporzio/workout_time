68a6c88816c53c997077aa4a452a68ba011ee97b
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2023-04-11 22:50:22 +0200

    da rivedere

--- lib/features/workout/presentation/cubit/single_workout/single_workout_cubit.dart
+++ lib/features/workout/presentation/cubit/single_workout/single_workout_cubit.dart
@@ -1,8 +1,10 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../domain/entities/workout.dart';
 
 part 'single_workout_state.dart';
 
 class SingleWorkoutCubit extends Cubit<SingleWorkoutState> {
-  SingleWorkoutCubit() : super(SingleWorkoutInitial());
+  SingleWorkoutCubit() : super(SingleWorkoutState.initial());
 }
--- lib/features/workout/presentation/cubit/single_workout/single_workout_state.dart
+++ lib/features/workout/presentation/cubit/single_workout/single_workout_state.dart
@@ -1,10 +1,17 @@
 part of 'single_workout_cubit.dart';
 
-abstract class SingleWorkoutState extends Equatable {
-  const SingleWorkoutState();
+class SingleWorkoutState extends Equatable {
+  final Workout? workout;
+  final int? elapsed;
+
+  const SingleWorkoutState({
+    this.workout,
+    this.elapsed,
+  });
+
+  factory SingleWorkoutState.initial() =>
+      const SingleWorkoutState(elapsed: 0, workout: null);
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [workout, elapsed];
 }
-
-class SingleWorkoutInitial extends SingleWorkoutState {}
